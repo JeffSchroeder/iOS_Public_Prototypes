ffdb98c0813ed80e7cf08bd20a1a042c021258b8
Jeff Schroeder <jeff@jeffschroeder.com>
2020-01-25 14:12:47 -0700

    Long Press update

--- LongPress/LongPress/ViewController.swift
+++ LongPress/LongPress/ViewController.swift
@@ -11,11 +11,15 @@ import UIKit
 class ViewController: UIViewController {
     
     var theTimer: Timer?
+    var whatsHappening = [String]()
+    
+    @IBOutlet weak var whatsHappeningTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        whatsHappening.append("Form Loaded")
         let longPress = UILongPressGestureRecognizer(target: self, action: #selector(self.processLongPress))
         longPress.minimumPressDuration = TimeInterval(0.5)
         if let recognizers = view.gestureRecognizers {
@@ -27,36 +31,71 @@ class ViewController: UIViewController {
     }
     
     @objc func processLongPress(gesture: UITapGestureRecognizer) {
+         print("\(whatsHappening.startIndex)")
         switch gesture.state {
+           
         case .possible:
-            print("began")
+            //whatsHappening.append("possible")
+            whatsHappening.insert("possible", at: whatsHappening.startIndex + 1)
+            print("possible")
             theTimer = nil
         case .began:
+            //whatsHappening.append("began")
+            whatsHappening.insert("began", at: whatsHappening.startIndex)
             print("began")
-            theTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(processSomething), userInfo: nil, repeats: false)
+            theTimer = Timer.scheduledTimer(timeInterval: 2.5, target: self, selector: #selector(processSomething), userInfo: nil, repeats: false)
         case .changed:
+            whatsHappening.insert("theTimer", at: whatsHappening.startIndex)
             print("changed")
             theTimer = nil
         case .ended:
+            guard let _ = theTimer else {
+                whatsHappening.insert("ended - already processed", at: whatsHappening.startIndex)
+                print("ended - already processed")
+                whatsHappeningTableView.reloadData()
+                return
+            }
+            whatsHappening.insert("ended", at: whatsHappening.startIndex)
             print("ended")
-            guard let _ = theTimer else { return }
             processSomething()
         case .cancelled:
+            whatsHappening.insert("cancelled", at: whatsHappening.startIndex)
             print("cancelled")
             theTimer = nil
         case .failed:
+            whatsHappening.insert("failed", at: whatsHappening.startIndex)
             print("failed")
             theTimer = nil
         @unknown default:()
-            
         }
+        whatsHappeningTableView.reloadData()
     }
     
     @objc func processSomething() {
         if self.theTimer != nil {
+            whatsHappening.insert("processSomething", at: whatsHappening.startIndex)
             print("processSomething")
             self.theTimer = nil
+            whatsHappeningTableView.reloadData()
         }
     }
+}
+extension ViewController: UITableViewDelegate {
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return whatsHappening.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WhatsHappeningCell", for: indexPath)
+        
+        cell.textLabel?.text = whatsHappening[indexPath.row]
+        return cell
+    }
+    
+    
 }
 
